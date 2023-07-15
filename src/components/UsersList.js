59c8eac2715eb067ca414bf1a12c9a27a10d9b18
AnjaP <treyjapan@pm.me>
2023-07-15 14:28:02 +0200

    feat:finish implementing thunks

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store/thunks/fetchUsers";
+
 
 function UserList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return 'Users List'
 }
 
