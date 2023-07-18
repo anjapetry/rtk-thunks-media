7b35ec22fb6c574b53eef1995aba0c89f2e51cbf
AnjaP <treyjapan@pm.me>
2023-07-18 18:43:38 +0200

    feat:add Skeleton loader

--- src/store/thunks/fetchUsers.js
+++
@@ -1,9 +0,0 @@
-import {createAsyncThunk} from '@reduxjs/toolkit';
-import axios from 'axios';
-
-export const fetchUsers = createAsyncThunk(
-    'users/fetch', async () => {
-        const response = await axios.get('http://localhost:3005/users');// axios is a library for making HTTP requests;
-        return response.data;
-    });
-    
