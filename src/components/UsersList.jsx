0ad2a601348b04b83ab21560afce6a674416ac6a
AnjaP <treyjapan@pm.me>
2023-07-19 18:46:27 +0200

    feat:handle errors with user creation

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,28 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
 import Button from './Button';
 import Skeleton from './Skeleton';
 
 function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUserError, setCreatingUserError] = useState(null);
   const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+   .unwrap()
+   .catch((err) => setLoadingUsersError(err))
+   .finally(() => setIsLoadingUsers(false));
   }, [dispatch]);
 
   const handleUserAdd = () => {
-    dispatch(addUser());
+    setIsCreatingUser(true);
+    dispatch(addUser())
+    .unwrap()
+    .catch(err => setCreatingUserError(err))
+    .finally(() => setIsCreatingUser(false));
   };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="w-full h-10" />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
@@ -40,7 +52,11 @@ function UsersList() {
     <div className='rounded-lg'>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button className="p-3 my-auto rounded-lg bg-slate-100" onClick={handleUserAdd}>+ Add User</Button>
+        { isCreatingUser 
+          ? 'Creating User ...'
+          :  <Button className="p-3 my-auto rounded-lg bg-slate-100" onClick={handleUserAdd}> + Add User</Button>
+        }
+        { creatingUserError && 'Error creating user ...'}
       </div>
       {renderedUsers}
     </div>
