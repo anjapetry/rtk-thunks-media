ef656c2e02a795b72561f685d8c7a502f1343c05
AnjaP <treyjapan@pm.me>
2023-07-28 17:03:18 +0200

    feat:change album data with mutations

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,10 +1,16 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
 import Button from "./Button";
 
 const AlbumsList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
   let content;
   if (isLoading) {
@@ -25,6 +31,7 @@ const AlbumsList = ({ user }) => {
   return <div>
     <div>
       Albums for {user.name}
+      <Button onClick={handleAddAlbum} className="rounded ml-3"> + Add Album</Button>
     </div>
     <div>
       {content}
--- src/store/apis/albumsApi.jsx
+++ src/store/apis/albumsApi.jsx
@@ -1,10 +1,23 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
 
 const albumsApi = createApi({
     reducerPath: 'albums',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3005' }),
     endpoints: (builder) => {
         return {
+            addAlbum: builder.mutation({
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        method: 'POST',
+                        body: {
+                        userId: user.id,
+                        title: faker.commerce.productName()
+                        },
+                    };
+                },
+            }),
             fetchAlbums: builder.query({
                 query: (user) => {
                     return {
@@ -20,5 +33,5 @@ const albumsApi = createApi({
  },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
